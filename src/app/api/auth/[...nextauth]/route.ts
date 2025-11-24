425f8945321f97fd0b8377dc2a8bd263d48b0e2e
H4K3R-TOOLS <141720322+H4K3R-TOOLS@users.noreply.github.com>
2025-11-24 00:57:08 -0800

    changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-const authOptions = {
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Provider from './provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +10,21 @@ export const metadata: Metadata = {
     description: 'Your synced gallery',
 }
 
-export default function RootLayout({
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const session = await getServerSession(authOptions)
+
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Provider session={session}>{children}</Provider>
+            </body>
         </html>
     )
 }
