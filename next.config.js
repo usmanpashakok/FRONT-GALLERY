615d55d947b5c627a53829712ef8edd4ddb25181
H4K3R-TOOLS <141720322+H4K3R-TOOLS@users.noreply.github.com>
2026-03-31 04:21:58 -0700

    w

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com'],
+        domains: ['pub-5b4a6b6f87d24e218dc9dcd6a47ec39b.r2.dev'],
         unoptimized: true,
     },
 }
